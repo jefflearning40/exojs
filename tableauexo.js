1bbf87c45ade4c7b9f9fca0a4c120ad5ed32ee88
ledeunf <JeanFrancois@gmail.com>
2025-04-30 11:46:14 +0200

    doublons

---
+++ tableauexo.js
@@ -0,0 +1,58 @@
+
+//----------------exo 1 ---------------------------------
+/*let animaux=["chien","chat","lapin"];
+let couleur=["rouge","vert","bleu"];
+console.log(couleur[2]);
+animaux.shift();//supprime le premier element de animaux
+animaux.unshift("hamster");//insere au debut du tableu un element
+console.log(animaux);
+//----------exo 2-------------------------------------
+animaux.push("perroquet");
+console.log(animaux);
+animaux.shift();
+console.log(animaux);
+console.log(animaux.includes("chat"));
+console.log(animaux.join(","));
+//-----------------------exo 3 -----------------------------
+let nombres = [];
+for (let i = 1; i < 11; i++) {
+    nombres.push(i);
+}
+console.log(nombres);
+console.log(nombres.length);*/
+// ------------------------exercice 4---------------------------
+let doublon = [];
+let single = [];
+let occurrences = {};
+
+
+for (let j = 0; j < 11; j++) {
+    let n = Math.floor(Math.random() * 10);
+    doublon.push(n);
+}
+
+console.log("Tableau initial doublon:", doublon);
+
+
+for (let a of doublon) {
+    if (occurrences[a]) {
+        occurrences[a]++;
+    } else {
+        occurrences[a] = 1;
+    }
+}
+
+
+doublon = [];
+
+
+for (let num in occurrences) {
+    if (occurrences[num] === 1) {
+        single.push(parseInt(num));
+    } else {
+        doublon.push(parseInt(num));
+    }
+}
+
+console.log("Tableau single (uniques):", single);
+console.log("Tableau doublon (doublons):", doublon);
